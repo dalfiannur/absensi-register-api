e1014157bacb95633e97c220dd092ba499241507
typeofname <dalfiannur@gmail.com>
2019-11-07 06:30:55 +0700

    Fix Heroku

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(json())
   app.use(urlencoded({ extended: false }))
-  await app.listen(3000);
+  app.enableCors()
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
